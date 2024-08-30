20242230a93c9ffe6cbf91d00e0313abbcc93205
Narendra <kumar964050@gmail.com>
2024-08-30 22:28:25 +0530

    add commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.jsx";
 // styles
 import "./index.css";
 import "aos/dist/aos.css";
